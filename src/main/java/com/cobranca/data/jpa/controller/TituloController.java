5d06b659890fbaf08a6447d704dfbfda73092ed8
Mikaelly Aguiar <mikaelly-aguiar@hotmail.com>
2016-07-01 00:07:55 -0300

    Plugin para calendário

--- src/main/java/com/cobranca/data/jpa/controller/TituloController.java
+++ src/main/java/com/cobranca/data/jpa/controller/TituloController.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
@@ -40,10 +41,15 @@ public class TituloController {
 			return CADASTRO_VIEW;
 		}
 		
-		titulos.save(titulo);			
-		attributes.addFlashAttribute("mensagem", "Titulo salvo com sucesso!");
+		try {
+			titulos.save(titulo);			
+			attributes.addFlashAttribute("mensagem", "Titulo salvo com sucesso!");
 		
-		return "redirect:/titulos/novo";
+			return "redirect:/titulos/novo";
+		} catch (DataIntegrityViolationException e ) {
+			erros.rejectValue("dataVencimento", null, "Formato de data inválido.");
+			return CADASTRO_VIEW;
+		}
 	}
 	
 	@RequestMapping
